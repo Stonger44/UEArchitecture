901b9430c7758dd07eacbfcfa7df3afbaa3d7ec4
Stonger <49844364+Stonger44@users.noreply.github.com>
2025-08-07 22:57:18 -0700

    Added Ship class
    Added ImpulseStrength property to Ship

---
+++ Source/UEArchitecture/Private/Ship.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ship.h"
+
+// Sets default values
+AShip::AShip()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AShip::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UE_LOG(LogTemp, Warning, TEXT("Impulse Strength: %f"), ImpulseStrength);
+
+}
+
+// Called every frame
+void AShip::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShip::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
